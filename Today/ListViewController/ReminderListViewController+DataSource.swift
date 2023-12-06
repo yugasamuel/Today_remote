aa5b7b1249abb3fe6b84256fbd72c301feb9cdfa
Yuga Samuel <yugasamuel2002@gmail.com>
2023-12-06 20:17:17 +0700

    Change the cell background color

--- Today/ListViewController/ReminderListViewController+DataSource.swift
+++ Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -19,5 +19,9 @@ extension ReminderListViewController {
         contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(
             forTextStyle: .caption1)
         cell.contentConfiguration = contentConfiguration
+        
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        backgroundConfiguration.backgroundColor = UIColor(red: 1.0, green: 0.8, blue: 0.9, alpha: 1.0)
+        cell.backgroundConfiguration = backgroundConfiguration
     }
 }
