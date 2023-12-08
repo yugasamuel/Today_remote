389097b92d1eeaca31a31300b9a315683ce8b195
Yuga Samuel <yugasamuel2002@gmail.com>
2023-12-08 16:41:51 +0700

    Apply discrete case in an enum for each row in detail view

---
+++ Today/DetailViewController/ReminderViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ReminderViewController.swift
+//  Today
+//
+//  Created by Yuga Samuel on 08/12/23.
+//
+
+import UIKit
+
+class ReminderViewController: UICollectionViewController {
+    var reminder: Reminder
+    
+    init(reminder: Reminder) {
+        self.reminder = reminder
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        listConfiguration.showsSeparators = false
+        let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+        super.init(collectionViewLayout: listLayout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Always initialize ReminderViewController using init(reminder:)")
+    }
+    
+    func text(for row: Row) -> String? {
+        switch row {
+        case .date: return reminder.dueDate.dayText
+        case .notes: return reminder.notes
+        case .time: return reminder.dueDate.formatted(date: .omitted, time: .shortened)
+        case .title: return reminder.title
+        }
+    }
+    
+}
