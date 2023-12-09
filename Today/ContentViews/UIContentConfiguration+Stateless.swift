2763f0721f022390ab3aaafe4e47b72b645ada5a
Yuga Samuel <yugasamuel2002@gmail.com>
2023-12-09 11:33:30 +0700

    Conform the text field to the content view protocol

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Yuga Samuel on 09/12/23.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
