399493dd6d95532cb5306cd082ce69d7bf8de4e8
Yuga Samuel <yugasamuel2002@gmail.com>
2023-12-10 13:00:49 +0700

    Create function to add new Reminder

--- Today/ListViewController/ReminderListViewControler+Actions.swift
+++ Today/ListViewController/ReminderListViewControler+Actions.swift
@@ -12,4 +12,21 @@ extension ReminderListViewController {
         guard let id = sender.id else { return }
         completeReminder(withId: id)
     }
+    
+    @objc func didPressAddButton(_ sender: UIBarButtonItem) {
+        let reminder = Reminder(title: "", dueDate: Date.now)
+        let viewController = ReminderViewController(reminder: reminder) { _ in }
+        viewController.isAddingNewReminder = true
+        viewController.setEditing(true, animated: false)
+        viewController.navigationItem.leftBarButtonItem = UIBarButtonItem(
+                    barButtonSystemItem: .cancel, target: self, action: #selector(didCancelAdd(_:)))
+        viewController.navigationItem.title = NSLocalizedString(
+                    "Add Reminder", comment: "Add Reminder view controller title")
+        let navigationController = UINavigationController(rootViewController: viewController)
+        present(navigationController, animated: true)
+    }
+    
+    @objc func didCancelAdd(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
 }
