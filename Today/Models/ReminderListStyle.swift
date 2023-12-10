13f206a7811a8cd449101e7e3080b382ef15f0eb
Yuga Samuel <yugasamuel2002@gmail.com>
2023-12-10 16:16:57 +0700

    Add filter feature according to duedate

---
+++ Today/Models/ReminderListStyle.swift
@@ -0,0 +1,26 @@
+//
+//  ReminderListStyle.swift
+//  Today
+//
+//  Created by Yuga Samuel on 10/12/23.
+//
+
+import Foundation
+
+enum ReminderListStyle: Int {
+    case today
+    case future
+    case all
+    
+    func shouldInclude(date: Date) -> Bool {
+        let isInToday = Locale.current.calendar.isDateInToday(date)
+        switch self {
+        case .today:
+            return isInToday
+        case .future:
+            return (date > Date.now) && !isInToday
+        case .all:
+            return true
+        }
+    }
+}
